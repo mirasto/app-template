aa90664b4816ac9bd298cfd6a4512533a48be1a1
mirasto <maksimorozov@protonmail.com>
2025-12-23 11:40:29 +0200

    remove comments

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,21 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint'; // Додаємо підтримку TS
-import prettierConfig from 'eslint-config-prettier'; // Тільки конфіг
+import tseslint from 'typescript-eslint'; 
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-  // 1. Ігнорування папок (замість globalIgnores)
   { ignores: ['dist', 'node_modules', 'build'] },
 
-  // 2. Базовий конфіг для JS та TS
   js.configs.recommended,
   ...tseslint.configs.recommended,
 
   {
-    // Оновлюємо розширення файлів
     files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      // Використовуємо TS парсер
+
       parser: tseslint.parser,
       parserOptions: {
         project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
@@ -32,12 +29,8 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      // Налаштування для невикористаних змінних (враховуючи TS)
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
   },
-
-  // 3. Prettier Config — ЗАВЖДИ ОСТАННІЙ
-  // Він просто вимикає правила ESLint, які конфліктують з Prettier
   prettierConfig,
 );
--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,17 @@
-// postcss.config.js
 export default {
   plugins: {
-    // 1. Покращує імпорти (якщо раптом використовуєш звичайні .css файли)
     'postcss-import': {},
-
-    // 2. Групує медіа-запити (дуже корисно для чистоти коду)
     'postcss-sort-media-queries': {
-      sort: 'mobile-first', // або 'desktop-first' залежно від твого підходу
+      sort: 'mobile-first',
     },
-
-    // 3. Твій поточний нормалайзер
     'postcss-normalize': {},
-
-    // 4. Сучасні фічі + автопрефікси
     'postcss-preset-env': {
       stage: 3,
       autoprefixer: {
-        flexbox: 'no-2009', // підтримка сучасного flexbox
+        flexbox: 'no-2009',
       },
       features: {
-        'custom-properties': false, // Vite/Sass і так з цим справляються
+        'custom-properties': false,
       },
     },
   },
